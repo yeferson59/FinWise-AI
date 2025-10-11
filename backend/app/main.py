4c3df976e9a07043a0d5cac5557804a083715015
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2025-10-11 00:01:18 -0500

    feat: add database initialization in FastAPI lifespan event

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,19 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.api.v1.router import router
 from app.config import get_settings
+from app.db.base import create_db_and_tables
 
 settings = get_settings()
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(router=router, prefix=settings.prefix_api)
 
