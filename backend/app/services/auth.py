32bc7be6328e95cff3110f7c979d761466984f85
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2025-10-14 12:51:42 -0500

    feat: store session data on login and set token expiry

--- backend/app/services/auth.py
+++ backend/app/services/auth.py
@@ -2,6 +2,11 @@ from app.services import user
 from app.schemas import auth
 from app.db.session import SessionDep
 from app.core.security import verify_password, create_token
+from app.models.auth import Session
+from datetime import datetime, timedelta
+from app.config import get_settings
+
+settings = get_settings()
 
 
 async def login(session: SessionDep, login_data: auth.Login) -> auth.LoginResponse:
@@ -13,7 +18,18 @@ async def login(session: SessionDep, login_data: auth.Login) -> auth.LoginRespon
     if not await verify_password(login_data.password, user_data.password):
         return auth.LoginResponse(success=False)
 
-    token = await create_token({"sub": user_data.id, "email": user_data.email})
+    token = await create_token({"sub": str(user_data.id), "email": user_data.email})
+
+    session_data = Session(
+        user_id=user_data.id,
+        token=token,
+        expires_at=datetime.now()
+        + timedelta(minutes=settings.access_token_expire_minutes),
+    )
+
+    session.add(session_data)
+    session.commit()
+    session.refresh(session_data)
 
     return auth.LoginResponse(
         success=True,
