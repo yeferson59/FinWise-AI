40fff3444a4c5f3abe693513e00c48351b494162
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2025-10-14 16:48:26 -0500

    fix: remove unnecessary default values from model fields

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -4,28 +4,25 @@ from .base import Base
 
 class User(Base, table=True):
     first_name: str = Field(
-        default=None,
         description="User's first name",
         nullable=False,
         min_length=2,
         max_length=100,
     )
     last_name: str = Field(
-        default=None,
         description="User's last name",
         nullable=False,
         min_length=2,
         max_length=100,
     )
     email: str = Field(
-        default=None,
         description="User's email address",
         unique=True,
         nullable=False,
         min_length=8,
         max_length=255,
     )
-    password: str = Field(
+    password: str | None = Field(
         default=None,
         description="User's password",
         nullable=False,
