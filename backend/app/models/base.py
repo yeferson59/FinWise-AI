59a4ca9898859792d8ce3a535669aae4c6ed2088
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2025-10-14 16:47:01 -0500

    feat: add BaseUuid model and uuid dependency for UUID primary keys

--- backend/app/models/base.py
+++ backend/app/models/base.py
@@ -1,12 +1,29 @@
 from sqlmodel import SQLModel, Field
 from datetime import datetime
+from uuid import uuid4, UUID
 
 
 class Base(SQLModel):
-    id: int = Field(default=None, primary_key=True)
+    id: int = Field(default=None, primary_key=True, description="Primary key of table")
     created_at: datetime = Field(
-        default=datetime.now(), description="Creation timestamp"
+        default_factory=datetime.now, description="Creation timestamp", nullable=False
     )
     updated_at: datetime = Field(
-        default=datetime.now(), description="Last update timestamp"
+        default_factory=datetime.now,
+        description="Last update timestamp",
+        nullable=False,
+    )
+
+
+class BaseUuid(SQLModel):
+    id: UUID = Field(
+        default_factory=uuid4, primary_key=True, description="Primary key of table"
+    )
+    created_at: datetime = Field(
+        default_factory=datetime.now, description="Creation timestamp", nullable=False
+    )
+    updated_at: datetime = Field(
+        default_factory=datetime.now,
+        description="Last update timestamp",
+        nullable=False,
     )
